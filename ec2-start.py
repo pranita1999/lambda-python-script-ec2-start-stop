d99cc85388a64088a7dbe57929981e31a9b83b0c
Pranita Gughane <69386793+pranita1999@users.noreply.github.com>
2023-08-31 18:00:23 +0530

    Update ec2-start.py

--- ec2-start.py
+++ ec2-start.py
@@ -1,10 +1,9 @@
 import boto3
 # change the region name below 
-region = 'us-east-1'
+region = 'us-west-2'
 #change the Instance ID 
-instances = ['i-064369a69363fa9f7',]
+instances = ['i-085c72a9d7e12f6b7',]
 ec2 = boto3.client('ec2', region_name=region)
-
 def lambda_handler(event, context):
     ec2.start_instances(InstanceIds=instances)
-    print('started your instances: ' + str(instances))
+    print('started your instances: ' + str(instances))  
